52da1f54625a0c6c3be849f78b8791ec81dfdf03
Lucas Daniel <ludaniel.sd@gmail.com>
2022-09-08 23:21:34 -0300

    adicionando a interação com o javascript

---
+++ app.js
@@ -0,0 +1,17 @@
+// você pode ativar o modo estrito, que reduz erros silenciosos, melhora o desempenho, fornece mais avisos e menos recursos não seguros.
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click' , function() {
+    document.body.classList.toggle('dark-theme');
+
+    var className = document.body.className;
+    if(className == "light-theme"){
+        this.textContent = "Dark";
+    }
+    else{
+        this.textContent = "Light";
+    }
+    console.log('current class name: '+ className);
+});
